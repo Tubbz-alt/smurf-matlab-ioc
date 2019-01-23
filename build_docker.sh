b2d4bc1fdc8c320e6d74e9969ce9aa23905fc2d8
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-01-22 17:58:33 -0800

    Print name of the docker image created

--- build_docker.sh
+++ build_docker.sh
@@ -6,3 +6,4 @@ app_version=$(git describe --tags --always)
 # Build the docker and tagged it with the application version
 docker build -t jesusvasquez333/smurf-matlab-ioc .
 docker tag jesusvasquez333/smurf-matlab-ioc jesusvasquez333/smurf-matlab-ioc:${app_version}
+printf "Docker image created: jesusvasquez333/smurf-matlab-ioc:${app_version}\n"
